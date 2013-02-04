e950cd9e3b59bfbfe692f810077fb8f073d4d22f
Hamdan1990 <hamdansiddiqui@hotmail.com>
2013-02-04 16:56:32 -0600

    Commenting the Functions

---
+++ Record.h
@@ -0,0 +1,12 @@
+#include "std_lib_facilities.h"
+
+#ifndef RECORD_H
+#define RECORD_H
+
+
+class Record {
+	Record();
+};
+
+
+#endif
--- database.h
+++ database.h
@@ -3,53 +3,53 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 
-class Record {
-	Record();
-};
-
-class Table {
-public:
-	// Need constructors for rows and columns. And empty constructor
-	Table();
-
-	// Constructor to take a list of attribute names and types
-	
-	// Add function that takes in a single attribute name and type 
-	// and adds a column to the end of the table with that new 
-	// attribute. Any entries currently in the table should get NULL
-	// for that entry
-
-	// A delete function that takes an attribute name and deletes it from the table
-
-	// An insert command that takes a record and adds it to the table
-
-	// A get attributes command that returns a list of the attributes and types for that table
-
-	// A get size command that returns the number of records in the table
-
-	// An iterator of some sort that can be used to return individual records from the table
-
-	// A rename attribute command that takes two names, and replaces the name for the attribute given by the first name with the second name
-
-	// A cross join command that takes two tables as input and produces one table as output
-
-	// Routines that take a single attribute name as input, and compute the following 
-	// sum, count (counts non-null entries only), Min, MAx
-
-	
-};
 
 class Database {
 	
 public: 
 	// Constructor function with no arguments. Creates Empty database
 	Database(); 
+	/*  //RETURN VALUE FOR THE Database() FUNCTION:
+	    no return value since it is a constructor
 
+		//FUNCTIONALITY 
+	    Initializes an empty Database
+
+		//PARAMETERS 
+		 Does not take any parameter
+	
+	*/
 	// Add table function that takes a single table and a name, and adds that table to the database
-	void add_table(String name);
+	void add_table( string Table, String Attribute);
+	
+	/*  //RETURN VALUE FOR THE Database() FUNCTION:
+	    no return value 
+
+		//FUNCTIONALITY 
+	    Add the attribute "Attribute" to the database "Table"
+
+		//PARAMETERS 
+		 Takes a string "Table" which is the name of a Database. 
+		 Takes a string "Attribute" and adds that attritube to the database "Table"  
+	
+	*/
+	
 
 	// Drop function that takes a table name and deletes it from the database
-	void drop_table(String name);
+	void drop_table(String Table);
+
+	/*  //RETURN VALUE FOR THE Database() FUNCTION:
+	    no return value 
+
+		//FUNCTIONALITY 
+	    Deletes the "Table" from the Database
+
+		//PARAMETERS 
+		 Takes a string "Table" which is the name of a Database instance 
+		   
+	
+	*/
+	
 
 	// List table function that returns a list of all table names in the database
 
